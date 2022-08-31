92ea30b36555ca10e27594e25e252384d9b2a58c
Jonas4884 <hei.jonatana@gmail.com>
2022-08-31 15:06:48 +0300

    feat: fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import {HomePage} from "./pages/HomePage/HomePage";
+import {HackWebProviders} from "./providers/HackWebProviders";
+import {ChakraProvider, theme} from "@chakra-ui/react";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home";
 
 export const App = () => {
   return (
-   <HomePage/>
+      <Router>
+        <HackWebProviders>
+          <ChakraProvider theme={theme}>
+
+            <Routes>
+              <Route path="/" element={<HomePage />}>
+                <Route index element={<Navigate to="dishes" />} />
+              </Route>
+            </Routes>
+
+          </ChakraProvider>
+        </HackWebProviders>
+      </Router>
   );
 }
