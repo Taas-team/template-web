699de95b7401ddd1fb5eabcda001a345ba1b36a7
Jonas4884 <hei.jonatana@gmail.com>
2022-09-01 10:39:58 +0300

    feat : add features

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Banner, Navbar } from './components';
 import Footer from './components/Footer';
 import {AuthProvider} from "../../providers/AuthProvider";
+import Features from "./components/Features";
 
 export const Landing = () => (
   <AuthProvider>
     <Navbar />
     <Banner />
-
+    <Features/>
     <Footer />
   </AuthProvider>
 );
